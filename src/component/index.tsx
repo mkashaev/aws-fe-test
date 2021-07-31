46e09c30d702e878b425006f14dba8d1ae612b54
Marat Kashaev <kashaevmt@gmail.com>
2021-08-01 02:11:53 +0300

    ADD tailwind

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -2,15 +2,24 @@ import { observer } from "mobx-react-lite";
 import { useContext } from "react";
 import { Context } from "../App";
 
+const classStyle: string =
+  "bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500";
+
 export const ClickerCom = observer(() => {
   const { click } = useContext(Context);
   return (
     <div>
       <p>{click.counter}</p>
       <p>Loading: {click.isLoading}</p>
-      <button onClick={() => click.inc()}>inc</button>
-      <button onClick={() => click.dec()}>dec</button>
-      <button onClick={() => click.asInc()}>as inc</button>
+      <button className={classStyle} onClick={() => click.inc()}>
+        inc
+      </button>
+      <button className={classStyle} onClick={() => click.dec()}>
+        dec
+      </button>
+      <button className={classStyle} onClick={() => click.asInc()}>
+        as inc
+      </button>
     </div>
   );
 });
